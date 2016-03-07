ed7a7819282f4955887f199114e22337997f9f12
Chahal Arora <chahal.arora@gmail.com>
2016-03-08 02:24:32 +0530

    code cleanup

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var app = require('http').createServer(handler),
-  io = require('socket.io').listen(app),
-  fs = require('fs'),
-  mysql = require('mysql'),
-  connectionsArray = [],
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Hello123',
-    database: 'socketTest',
-    port: 3306
-  }),
-  POLLING_INTERVAL = 3000,
-  pollingTimer;
+    io = require('socket.io').listen(app),
+    fs = require('fs'),
+    mysql = require('mysql'),
+    connectionsArray = [],
+    connection = mysql.createConnection({
+        host: 'localhost',     //Database username
+        user: 'root',          //Database username
+        password: 'Hello123',  //Database password
+        database: 'socketTest', //Database name
+        port: 3306
+    }),
+    POLLING_INTERVAL = 3000,
+    pollingTimer;
 
 // If there is an error connecting to the database
 connection.connect(function(err) {
-  // connected! (unless `err` is set)
-  if (err) {
-    console.log(err);
-  }
+    // connected! (unless `err` is set)
+    if (err) {
+        console.log(err);
+    }
 });
 
 // creating the server ( localhost:8000 )
@@ -26,164 +26,110 @@ app.listen(8000);
 
 // on server started we can load our client.html page
 function handler(req, res) {
-  fs.readFile(__dirname + '/client.html', function(err, data) {
-    if (err) {
-      console.log(err);
-      res.writeHead(500);
-      return res.end('Error loading client.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+    fs.readFile(__dirname + '/client.html', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.writeHead(500);
+            return res.end('Error loading client.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
 }
-
-/*
- *
- * HERE IT IS THE COOL PART
- * This function loops on itself since there are sockets connected to the page
- * sending the result of the database query after a constant interval
- *
- */
 
 var pollingLoop = function() {
 
-  // Doing the database query
-  var query = connection.query('SELECT * FROM users'),
-    users = []; // this array will contain the result of our db query
+    // Doing the database query
+    var query = connection.query('SELECT * FROM users'),
+        users = []; // this array will contain the result of our db query
 
-  // setting the query listeners
-  query
-    .on('error', function(err) {
-      // Handle error, and 'end' event will be emitted after this as well
-      console.log(err);
-      updateSockets(err);
-    })
-    .on('result', function(user) {
-      // it fills our array looping on each user row inside the db
-      users.push(user);
-    })
-    .on('end', function() {
-      // loop on itself only if there are sockets still connected
-      if (connectionsArray.length) {
+    // setting the query listeners
+    query
+        .on('error', function(err) {
+            // Handle error, and 'end' event will be emitted after this as well
+            console.log(err);
+            updateSockets(err);
+        })
+        .on('result', function(user) {
+            // it fills our array looping on each user row inside the db
+            users.push(user);
+        })
+        .on('end', function() {
+            // loop on itself only if there are sockets still connected
+            if (connectionsArray.length) {
 
-        pollingTimer = setTimeout(pollingLoop, POLLING_INTERVAL);
+                pollingTimer = setTimeout(pollingLoop, POLLING_INTERVAL);
 
-        updateSockets({
-          users: users
+                updateSockets({
+                    users: users
+                });
+            } else {
+
+                console.log('The server timer was stopped because there are no more socket connections on the app')
+
+            }
         });
-      } else {
-
-        console.log('The server timer was stopped because there are no more socket connections on the app')
-
-      }
-    });
 };
 
 
 var getUsersData = function() {
 
-  // Doing the database query
-  var query = connection.query('SELECT * FROM users'),
-    users = []; // this array will contain the result of our db query
+    // Doing the database query
+    var query = connection.query('SELECT * FROM users'),
+        users = []; // this array will contain the result of our db query
 
-  // setting the query listeners
-  query
-    .on('error', function(err) {
-      // Handle error, and 'end' event will be emitted after this as well
-      console.log(err);
-      sendUsersData(err);
-    })
-    .on('result', function(user) {
-      // it fills our array looping on each user row inside the db
-      users.push(user);
-    })
-    .on('end', function() {
-      // loop on itself only if there are sockets still connected
+    // setting the query listeners
+    query
+        .on('error', function(err) {
+            // Handle error, and 'end' event will be emitted after this as well
+            console.log(err);
+            sendUsersData(err);
+        })
+        .on('result', function(user) {
+            // it fills our array looping on each user row inside the db
+            users.push(user);
+        })
+        .on('end', function() {
+            // loop on itself only if there are sockets still connected
 
-        sendUsersData({
-          users: users
+            sendUsersData({
+                users: users
+            });
         });
-    });
 };
 
-// var getFieldData = function() {
-
-//   // Doing the database query
-//   var query = connection.query('SELECT column_name FROM information_schema.columns WHERE table_name = "users"'),
-//     fields = []; // this array will contain the result of our db query
-
-//   // setting the query listeners
-//   query
-//     .on('error', function(err) {
-//       // Handle error, and 'end' event will be emitted after this as well
-//       console.log(err);
-//       sendFieldData(err);
-//     })
-//     .on('result', function(field) {
-//       // it fills our array looping on each user row inside the db
-//       fields.push(field);
-//     })
-//     .on('end', function() {
-//       // loop on itself only if there are sockets still connected
-
-//         sendFieldData({
-//           fileds: fields
-//         });
-//     });
-// };
-
+var updateSockets = function(data) {
+    // sending new data to all the sockets connected
+    connectionsArray.forEach(function(tmpSocket) {
+        tmpSocket.volatile.emit('notification', data);
+    });
+};
+var sendUsersData = function(data) {
+    connectionsArray.forEach(function(tmpSocket) {
+        tmpSocket.emit('ehlo', data);
+    });
+};
 
 // creating a new websocket to keep the content updated without any AJAX request
 io.sockets.on('connection', function(socket) {
 
-  console.log('Number of connections:' + connectionsArray.length);
-  // starting the loop only if at least there is one user connected
-  
-  if (!connectionsArray.length) {
-    getUsersData()
-    pollingLoop();
-  }
+    console.log('Number of connections:' + connectionsArray.length);
+    // starting the loop only if at least there is one user connected
 
-  socket.on('disconnect', function() {
-    var socketIndex = connectionsArray.indexOf(socket);
-    console.log('socketID = %s got disconnected', socketIndex);
-    if (~socketIndex) {
-      connectionsArray.splice(socketIndex, 1);
+    if (!connectionsArray.length) {
+        getUsersData()
+        pollingLoop();
     }
-  });
 
-  console.log('A new socket is connected!');
-  connectionsArray.push(socket);
+    socket.on('disconnect', function() {
+        var socketIndex = connectionsArray.indexOf(socket);
+        console.log('socketID = %s got disconnected', socketIndex);
+        if (~socketIndex) {
+            connectionsArray.splice(socketIndex, 1);
+        }
+    });
+
+    console.log('A new socket is connected!');
+    connectionsArray.push(socket);
 
 });
-
-var updateSockets = function(data) {
-  // adding the time of the last update
-  data.time = new Date();
-  console.log('Pushing new data to the clients connected ( connections amount = %s ) - %s', connectionsArray.length , data.time);
-  // sending new data to all the sockets connected
-  connectionsArray.forEach(function(tmpSocket) {
-    tmpSocket.volatile.emit('notification', data);
-  });
-};
-var sendUsersData = function(data) {
-  // adding the time of the last update
-  data.time = new Date();
-  console.log('Pushing new data to the clients connected ( connections amount = %s ) - %s', connectionsArray.length , data.time);
-  // sending new data to all the sockets connected
-  console.log(data)
-  connectionsArray.forEach(function(tmpSocket) {
-    tmpSocket.emit('ehlo', data);
-  });
-};
-// var sendUsersData = function(data) {
-//   // adding the time of the last update
-//   data.time = new Date();
-//   console.log('Pushing new data to the clients connected ( connections amount = %s ) - %s', connectionsArray.length , data.time);
-//   // sending new data to all the sockets connected
-//   console.log(data)
-//   connectionsArray.forEach(function(tmpSocket) {
-//     tmpSocket.emit('ehlo', data);
-//   });
-// };
-console.log('Please use your browser to navigate to http://localhost:8000');
